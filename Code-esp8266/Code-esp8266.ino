ef8eab0377ecf05d4f2a0595d1f0e4074bee0ca4
Thoradur <91309831+Thoradur@users.noreply.github.com>
2024-04-25 09:28:03 +0200

    updated printscreen code

--- Code-esp8266/Code-esp8266.ino
+++ Code-esp8266/Code-esp8266.ino
@@ -13,22 +13,42 @@ void setup() {
   //Constuct LCD screen (16 = widht, 2 = height *in chars)
   lcd.begin(16,2);
   lcd.init();
+  
 
-  // Turn on the backlight.
-  lcd.backlight();
+  // // Move the cursor characters to the right and
+  // // zero characters down (line 1).
+  // lcd.setCursor(5, 0);
 
-  // Move the cursor characters to the right and
-  // zero characters down (line 1).
-  lcd.setCursor(5, 0);
+  // // Print HELLO to the screen, starting at 5,0.
+  // lcd.print("HELLO");
 
-  // Print HELLO to the screen, starting at 5,0.
-  lcd.print("HELLO");
-
-  // Move the cursor to the next line and print
-  // WORLD.
-  lcd.setCursor(5, 1);      
-  lcd.print("WORLD");
+  // // Move the cursor to the next line and print
+  // // WORLD.
+  // lcd.setCursor(5, 1);      
+  // lcd.print("WORLD");
+  
 }
 
 void loop() {
+  stringToLCD("Hello World",5000);
+
+
+
+}
+
+
+void stringToLCD(char* theText, int time){ //theText is what text that should be written on the screen and time is for how long it should be there in ms
+  
+  lcd.backlight(); // Turn on the backlight.
+
+  
+  //print text
+  lcd.print(theText);
+
+
+  delay(time); //time to show text
+  lcd.noBacklight(); // Turn off the backlight.
+  lcd.clear(); //clears screen
+
+
 }
