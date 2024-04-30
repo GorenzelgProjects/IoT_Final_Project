65bcfced81b412ab8128a4d766554f9376c15cc2
Rasmus2001 <RasmusSS2001@gmail.com>
2024-04-30 15:45:19 +0200

    Update to code

--- Code-esp8266/Code-esp8266.ino
+++ Code-esp8266/Code-esp8266.ino
@@ -1,33 +1,39 @@
-//LIBARIES
+#include <ESP8266WiFi.h>
 #include <Wire.h>
-#include <hd44780.h>                       // main hd44780 header
-#include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
-
-#include <SPI.h> //RFID
-#include <MFRC522.h> //RFID
-#include <Servo.h> //ServoMotor
-
-#include <WiFi.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
 #include "ThingSpeak.h"
 #include "secrets.h"
 
-//-----------------------------------------------------//
-//              WIFI / ThingSpeak / Sound
-//-----------------------------------------------------//
-int led = 10; //SDD3
-int sound_digital = 9; //////MAYBE need to be changed to 9 which is pin SDD2
-int sound_analog = A0;   
+#define RST_PIN D0
+#define SS_PIN D8
+#define BUTTON_PIN D4
+#define LED_PIN D1    // Adjust pin number
+#define SOUND_DIGITAL D2  // Adjust pin number
+#define SOUND_ANALOG A0
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+Servo servo;
+
+WiFiClient client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char* myWriteAPIKey = SECRET_WRITE_APIKEY;
+const char* myReadAPIKey = SECRET_READ_APIKEY;
+char ssid[] = SECRET_SSID;   // your network SSID (name)
+char pass[] = SECRET_PASS;   // your network password
+
+int storedUIDs = 0;
+#define MAX_UIDS 10
+byte authorizedUIDs[MAX_UIDS][4];
 
 const int ARRAY_LENGTH = 64;
 const int SAMPLE_TIME = 10;
 const int RECORD = 5000;
 
-// Time interval between each recordings (10 secs)
 int startRecord = 5000;
-
-// Time interval for the actual recording (5 secs)
 int stopRecord = startRecord + 5000;
-
 int recordStarted = 0;
 
 unsigned long millisCurrent = 0;
@@ -39,149 +45,165 @@ int sampleBufferValue = 0;
 int count_zero = 0;
 int array_count = 0;
 
-int RFID = 1; //VED IKKE OM VI SKAL BRUGE LÆNGERE
-
 String resultData = "";
 
 int currentSample = 1;
 int inverseSample = -1;
 
-unsigned long myChannelNumber = SECRET_CH_ID;
-const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
-
-char ssid[] = SECRET_SSID;   // your network SSID (name)
-char pass[] = SECRET_PASS;   // your network password
-
-
-//-----------------------------------------------------//
-//                 RFID / Servo
-//-----------------------------------------------------//
-#define MAX_UIDS 10   //Maximum number of storable UIDs
-byte authorizedUIDs[MAX_UIDS][4];  //Array to store multiple UIDs
-int storedUIDs = 0;  //Counter for the number of UIDs stored
-
-#define RST_PIN D0          //For RFID
-#define SS_PIN D8           //For RFID
-#define BUTTON_PIN D4       //Pin connected to the button
-
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
-Servo servo;
-
-//-----------------------------------------------------//
-//                 LCD
-//-----------------------------------------------------//
-// Construct an LCD object and pass it the 
-// I2C address, width (in characters) and
-// height (in characters). Depending on the
-// Actual device, the IC2 address may change. FOUND on address 0x27
-// LiquidCrystal_I2C lcd(0x27, 16, 2);
-hd44780_I2Cexp lcd;
-unsigned long lcdTimer = 0;
-
-
 void setup() {
-  //-----------------------------------------------------//
-  //                 WIFI / ThingSpeak / Sound
-  //-----------------------------------------------------//
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client);
-  pinMode(led, OUTPUT);
-  pinMode(sound_digital, INPUT);
 
-  //-----------------------------------------------------//
-  //                 RFID / Servo
-  //-----------------------------------------------------//
-  //Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SOUND_DIGITAL, INPUT);
+
   SPI.begin();
   mfrc522.PCD_Init();
-  servo.attach(D3);
+  servo.attach(D3);  // Confirm pin number
   servo.write(0);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
+  Serial.println("Setup complete.");
 
-  //-----------------------------------------------------//
-  //                 RFID / Servo
-  //-----------------------------------------------------//
-  Serial.begin(115200);
-  pinMode(led, OUTPUT);
-  pinMode(sound_digital, INPUT);
-
-
-
-  ThingSpeak.begin(client);
-
-  delay(100);
-  
-
-  
-  //-----------------------------------------------------//
-  //                 LCD
-  //-----------------------------------------------------//
-  lcd.begin(16,2);
-  //stringToLCD("Hello World", 5000); test print text
 }
 
 void loop() {
-  // Connect or reconnect to WiFi
+  handleWiFi();
+  handleRFIDandSound();
+}
+
+void handleWiFi() {
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(SECRET_SSID);
+    Serial.print("Connecting to SSID: ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
     while (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
       Serial.print(".");
-      delay(5000);
+      delay(500);
     }
     Serial.println("\nConnected.");
-  }else {
-    int val_digital = digitalRead(sound_digital);
-    int val_analog = analogRead(sound_analog);
+  }
+}
+
+void handleRFIDandSound() {
+  int val_digital = digitalRead(SOUND_DIGITAL);
+  int val_analog = analogRead(SOUND_ANALOG);
+
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    delay(500);
+    addNewChip();
+  }
+
+  if (val_digital == 1) {
+    sampleBufferValue++;
+  }
+
+  if (access()) {
+    Serial.println("Card Accepted");
+    delay(3000);
+    audio();
+    delay(10000);
+    //int clap = 1;
+    int clap = ThingSpeak.readIntField(myChannelNumber,2,myReadAPIKey);
+    Serial.println(clap);
+    if (clap == 1){
+      Serial.println("Clap Detected");
+      rotate(90);
+    }
+    else {
+      Serial.println("No clap detected");
+    }
+
+    // Handle additional logic here
+  } else {
+    pass;
+    //Serial.println("Card Denied");
+  }
+}
+
+bool access() {
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    return false;
+  }
+  if (isUIDInAccessList(mfrc522.uid.uidByte)) {
+    Serial.println("Access Granted");
+    mfrc522.PICC_HaltA();
+    return true;
+  }
+  Serial.println("Access Denied");
+  mfrc522.PICC_HaltA();
+  return false;
+}
+
+void addNewChip() {
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial() && storedUIDs < MAX_UIDS) {
+    for (int i = 0; i < 4; i++) {
+      authorizedUIDs[storedUIDs][i] = mfrc522.uid.uidByte[i];
+    }
+    storedUIDs++;
+    Serial.println("New chip added for access.");
+  }
+}
+
+bool isUIDInAccessList(byte* uid) {
+  for (int i = 0; i < storedUIDs; i++) {
+    bool match = true;
+    for (int j = 0; j < 4; j++) {
+      if (uid[j] != authorizedUIDs[i][j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void rotate(int angle) {
+    for(int pos = 20; pos < angle; pos ++) {  // goes from 0 degrees to 180 degrees
+                                    // in steps of 1 degree
+      servo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(30);}
+    //servo.write(angle);
+    delay(3000);
+    for(int pos = 20; pos < angle; pos ++) {  // goes from 0 degrees to 180 degrees
+                                    // in steps of 1 degree
+      servo.write(-pos);              // tell servo to go to position in variable 'pos'
+      delay(30);}
+    Serial.println("Servo should rotat");
+}
+
+void audio() {
+  millisStart = millis();
+  millisCurrent = millisStart;
+  millisLast = millisCurrent;
+
+  int responds = 0;
+  Serial.println("START RECORDING");
+  while (responds == 0){
+    digitalWrite (LED_PIN, HIGH);
+    int val_digital = digitalRead(SOUND_DIGITAL);
+    //int val_analog = analogRead(sound_analog);
     int result = val_digital;
-
-    //checks RFID 
-    // if (access()) {
-    //   rotate(90);  //Access granted, open the door
-    // }
-
-    //add new user/card
-    if (digitalRead(BUTTON_PIN) == LOW) {  //Check if the button is pressed
-      delay(500);
-    
-      addNewChip();  //Add a new RFID chip if pressed
-      stringToLCD("New User Added", 5000);
-    }
-
-
-    //Clean lcd screen
-    if (lcdTimer < millis()){
-      lcd.noBacklight(); // Turn off the backlight.
-      lcd.clear(); //clears screen
-    }
 
     if (val_digital == 1) {
       sampleBufferValue++;
     }
-    //checks rfid then sound
-    if (access()){
-      stringToLCD("Card Accepted", 5000);//CARD ACCEPTED - u might want to play with time
-      millisCurrent = millis();
-      millisElapsed = millisCurrent - millisLast;
-      if (recordStarted == 0) { 
-        stopRecord = millisCurrent + RECORD;
-        recordStarted = 1;
+    delay(1);
+    //Serial.println("SOUND");
+    if (millisElapsed > SAMPLE_TIME){
+      //Serial.println("SOUND");
+      if (sampleBufferValue*(-1) < 0){
+        currentSample = (sampleBufferValue*(-1)+1)-111;
+        inverseSample = currentSample * (-1);
       }
-      if (millisCurrent - millisStart > startRecord){
-        digitalWrite (led, HIGH);
-        if (millisElapsed > SAMPLE_TIME){
-
-          if (sampleBufferValue*(-1) < 0){
-            currentSample = (sampleBufferValue*(-1)+1)-111;
-            inverseSample = currentSample * (-1);
-
-          }
-          else {
-            currentSample = (sampleBufferValue);
-            inverseSample = currentSample * (-1);
-          }
+      else {
+        currentSample = (sampleBufferValue);
+        inverseSample = currentSample * (-1);
+      }
 
           //---------------------------------------------------------------------------
           // DETTE SKAL KUN BRUGES TIL POTENTIOMETER  (MELLEM 1950 - 2000 på Y-aksen i serial-plotter)
@@ -194,136 +216,64 @@ void loop() {
           //Serial.println(inverseSample);
           //---------------------------------------------------------------------------
 
-          sampleBufferValue = 0;
-          millisLast = millisCurrent;
+      sampleBufferValue = 0;
+      //millisLast = millisCurrent;
 
-          if (inverseSample == 0){
-            count_zero ++;
-          }
-          else {
+      if (inverseSample == 0){
+        count_zero ++;
+      }
+      else {
 
-            if (array_count < ARRAY_LENGTH){
-              count_zero = count_zero * (-1);
-              if (count_zero != 0){
-                resultData.concat(String(count_zero));
-                resultData.concat(String(","));
-              };
-              resultData.concat(String(inverseSample));
-              resultData.concat(String(","));
-            }
-
-            count_zero = 0;
-            array_count ++;
-            array_count ++;
-          }
-
+        if (array_count < ARRAY_LENGTH){
+          count_zero = count_zero * (-1);
+          if (count_zero != 0){
+            resultData.concat(String(count_zero));
+            resultData.concat(String(","));
+          };
+          resultData.concat(String(inverseSample));
+          resultData.concat(String(","));
         }
-        if (millisCurrent - millisStart > stopRecord){
-          millisStart = millisCurrent;
 
-          //---------------------------------------------------------------------------
-          // DETTE SKAL BRUGES TIL SELVE DATAINDSAMLING
-          Serial.println(resultData);
-          //---------------------------------------------------------------------------
-
-          int httpCode = ThingSpeak.writeField(myChannelNumber, 1, resultData, myWriteAPIKey);
-
-          if (httpCode == 200) {
-            Serial.println("Channel write successful.");
-          }
-          else {
-            Serial.println("Problem writing to channel. HTTP error code " + String(httpCode));
-          }
-
-          digitalWrite (led, LOW);
           count_zero = 0;
-          array_count = 0;
-          resultData = "";
-          RFID = 0; //VED IKKE OM VI SKAL BRUGE LÆNGERE
-          recordStarted = 0;
-        }
+          array_count ++;
+          array_count ++;
       }
+
+      millisLast = millisCurrent;
     }
+    
+    
+
+    if (millisCurrent - millisStart > RECORD){
+      millisStart = millisCurrent;
+      responds = 1;
+    }
+    millisCurrent = millis();
+    millisElapsed = millisCurrent - millisLast;
+    //Serial.println(millisElapsed);
+  
+
   }
-  
-  
+    //---------------------------------------------------------------------------
+    // DETTE SKAL BRUGES TIL SELVE DATAINDSAMLING
+    digitalWrite (LED_PIN, LOW);
+    Serial.println("DONE RECORDING - SENDING TO THINKSPEAK");
+    Serial.println(resultData);
+    //---------------------------------------------------------------------------
 
-}//LOOP END
+    int httpCode = ThingSpeak.writeField(myChannelNumber, 1, resultData, myWriteAPIKey);
 
+    if (httpCode == 200) {
+      Serial.println("Channel write successful.");
+    }
+    else {
+      Serial.println("Problem writing to channel. HTTP error code " + String(httpCode));
+    }
 
-
-
-
-
-//Here is the functions used
-bool access() {
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-      return false;
-  }
-
-  if (!mfrc522.PICC_ReadCardSerial()) {
-      return false;
-  }
-
-  if (isUIDInAccessList(mfrc522.uid.uidByte)) {
-      Serial.println("Access Granted");
-      mfrc522.PICC_HaltA();
-      return true;
-  } else {
-      Serial.println("Access Denied");
-      mfrc522.PICC_HaltA();
-      return false;
-  }
-}
-
-void openLock(int angle) {
-  
-  servo.write(angle);
-  delay(3000); //MIGHT WANT TO INCREASE TIME
-  
-  servo.write(0);
-}
-
-void addNewChip() {
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial() && storedUIDs < MAX_UIDS) {
-      // Add new UID to the list
-      for (int i = 0; i < 4; i++) {
-          authorizedUIDs[storedUIDs][i] = mfrc522.uid.uidByte[i];
-      }
-      storedUIDs++;  //Increment the count of stored UIDs
-      Serial.println("New chip added for access.");
-  }
-}
-
-bool isUIDInAccessList(byte* uid) {
-  for (int i = 0; i < storedUIDs; i++) {
-      bool match = true;
-      for (int j = 0; j < 4; j++) {
-          if (uid[j] != authorizedUIDs[i][j]) {
-              match = false;
-              break;
-          }
-      }
-      if (match) {
-          return true;
-      }
-  }
-  return false;
-}
-
-void stringToLCD(char* theText, int time){ //theText is what text that should be written on the screen and time is for how long it should be there in ms
-  lcdTimer = millis()+time;
-  
-  
-  lcd.backlight(); // Turn on the backlight.
-  
-  //print text
-  lcd.print(theText);
-  lcd.setCursor(0,0);
-
-  
-
-
-
-
+    
+    count_zero = 0;
+    array_count = 0;
+    resultData = "";
+    //RFID = 0;
+    recordStarted = 0;
 }
---
+++ Code-esp8266/secrets.h
@@ -0,0 +1,9 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Rasmus - iPhone"                                // replace MySSID with your WiFi network name
+#define SECRET_PASS "Hellothere"                            // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2522707                                 // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "G5WG42VHHEELJ3I4"                           // replace XYZ with your channel write API Key
+#define SECRET_READ_APIKEY "8KDFZQ0JZEUZTZ2S"                    //for read
